4e76bcf058188172e83de4506904262eb6100eef
Alderi-Junior <alderipasajunior@hotmail.com>
2024-11-13 11:02:44 -0300

    Created class Positon

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,14 @@
 package application;
 
 
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("É CHESS MANO");
+        Position pos = new Position(5, 6);
+        System.out.println(pos);
+
     }
 }
